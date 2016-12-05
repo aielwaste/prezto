3f52e12d5116cdba60897d83f936960de89f6ba0
Andrew Bell <andrewbell8@gmail.com>
2016-12-05 07:02:48 -0600

    Added Readmes Dir

--- init.zsh
+++ init.zsh
@@ -3,14 +3,14 @@
 #
 # Authors:
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
-#   Andrew Bell <andrewbell at gmail dot com>
+#   Andrew Bell <andrewbell8@gmail.com>
 #
 
 #
 # Version Check
 #
 
-# Check for the minimum supported version.
+#=== Check for the minimum supported version.
 min_zsh_version='4.3.17'
 if ! autoload -Uz is-at-least || ! is-at-least "$min_zsh_version"; then
   print "prezto: old shell detected, minimum required: $min_zsh_version" >&2
@@ -22,31 +22,31 @@ unset min_zsh_version
 # Module Loader
 #
 
-# Loads Prezto modules.
+#=== Loads Prezto modules.
 function pmodload {
   local -a pmodules
   local pmodule
   local pfunction_glob='^([_.]*|prompt_*_setup|README*)(-.N:t)'
 
-  # $argv is overridden in the anonymous function.
+  #=== $argv is overridden in the anonymous function.
   pmodules=("$argv[@]")
 
-  # Add functions to $fpath.
+  #=== Add functions to $fpath.
   fpath=(${pmodules:+${ZDOTDIR:-$HOME}/.zprezto/modules/${^pmodules}/functions(/FN)} $fpath)
 
   function {
     local pfunction
 
-    # Extended globbing is needed for listing autoloadable function directories.
+    #=== Extended globbing is needed for listing autoloadable function directories.
     setopt LOCAL_OPTIONS EXTENDED_GLOB
 
-    # Load Prezto functions.
-    for pfunction in ${ZDOTDIR}/.zprezto/modules/${^pmodules}/functions/$~pfunction_glob; do
+    #=== Load Prezto functions.
+    for pfunction in ${ZDOTDIR:-$HOME}/.zprezto/modules/${^pmodules}/functions/$~pfunction_glob; do
       autoload -Uz "$pfunction"
     done
   }
 
-  # Load Prezto modules.
+  #=== Load Prezto modules.
   for pmodule in "$pmodules[@]"; do
     if zstyle -t ":prezto:module:$pmodule" loaded 'yes' 'no'; then
       continue
@@ -54,25 +54,25 @@ function pmodload {
       print "$0: no such module: $pmodule" >&2
       continue
     else
-      if [[ -s "${ZDOTDIR}/.zprezto/modules/$pmodule/init.zsh" ]]; then
-        source "${ZDOTDIR}/.zprezto/modules/$pmodule/init.zsh"
+      if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/init.zsh" ]]; then
+        source "${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/init.zsh"
       fi
 
       if (( $? == 0 )); then
         zstyle ":prezto:module:$pmodule" loaded 'yes'
       else
-        # Remove the $fpath entry.
-        fpath[(r)${ZDOTDIR}/.zprezto/modules/${pmodule}/functions]=()
+        #=== Remove the $fpath entry.
+        fpath[(r)${ZDOTDIR:-$HOME}/.zprezto/modules/${pmodule}/functions]=()
 
         function {
           local pfunction
 
-          # Extended globbing is needed for listing autoloadable function
-          # directories.
+          #=== Extended globbing is needed for listing autoloadable function
+          #=== directories.
           setopt LOCAL_OPTIONS EXTENDED_GLOB
 
-          # Unload Prezto functions.
-          for pfunction in ${ZDOTDIR}/.zprezto/modules/$pmodule/functions/$~pfunction_glob; do
+          #=== Unload Prezto functions.
+          for pfunction in ${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/functions/$~pfunction_glob; do
             unfunction "$pfunction"
           done
         }
@@ -84,32 +84,31 @@ function pmodload {
 }
 
 #
-# Prezto Initialization
+# PREZTO INITIALIZATION
 #
 
-# Source the Prezto configuration file.
-if [[ -s "${HOME}/.zpreztorc" ]]; then
-  source "${HOME}/.zpreztorc"
+#=== Source the Prezto configuration file.
+if [[ -s "${ZDOTDIR:-$HOME}/.zpreztorc" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zpreztorc"
 fi
 
-# Disable color and theme in dumb terminals.
+#=== Disable color and theme in dumb terminals.
 if [[ "$TERM" == 'dumb' ]]; then
   zstyle ':prezto:*:*' color 'no'
   zstyle ':prezto:module:prompt' theme 'off'
 fi
 
-# Load Zsh modules.
+#=== Load Zsh modules.
 zstyle -a ':prezto:load' zmodule 'zmodules'
 for zmodule ("$zmodules[@]") zmodload "zsh/${(z)zmodule}"
 unset zmodule{s,}
 
-# Autoload Zsh functions.
+#=== Autoload Zsh functions.
 zstyle -a ':prezto:load' zfunction 'zfunctions'
 for zfunction ("$zfunctions[@]") autoload -Uz "$zfunction"
 unset zfunction{s,}
 
-# Load Prezto modules.
+#=== Load Prezto modules.
 zstyle -a ':prezto:load' pmodule 'pmodules'
 pmodload "$pmodules[@]"
 unset pmodules
-
