5adcad23a295b082a242a0e853534a20d464b5f3
Andrew Bell <fr1v@frivbook.local>
2017-03-02 18:59:55 -0600

    added noconfirm to pacman module

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -20,6 +20,7 @@ fi
 
 # Get the Pacman frontend.
 zstyle -s ':prezto:module:pacman' frontend '_pacman_frontend'
+zstyle -s ':prezto:module:pacman' noconfirm '_pacman_noconfirm'
 
 if (( $+commands[$_pacman_frontend] )); then
   alias pacman="$_pacman_frontend"
@@ -32,6 +33,10 @@ else
   _pacman_sudo='sudo '
 fi
 
+if (( $+commands[$_pacman_noconfirm] )); then
+  _pacman_noconfirm='--noconfirm'
+fi
+
 #
 # Aliases
 #
@@ -40,16 +45,16 @@ fi
 alias pac="${_pacman_frontend}"
 
 # Installs packages from repositories.
-alias paci="${_pacman_sudo}${_pacman_frontend} --sync"
+alias paci="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --sync"
 
 # Installs packages from files.
-alias pacI="${_pacman_sudo}${_pacman_frontend} --upgrade"
+alias pacI="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --upgrade"
 
 # Removes packages and unneeded dependencies.
-alias pacx="${_pacman_sudo}${_pacman_frontend} --remove"
+alias pacx="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --remove"
 
 # Removes packages, their configuration, and unneeded dependencies.
-alias pacX="${_pacman_sudo}${_pacman_frontend} --remove --nosave --recursive"
+alias pacX="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --remove --nosave --recursive"
 
 # Displays information about a package from the repositories.
 alias pacq="${_pacman_frontend} --sync --info"
@@ -72,13 +77,54 @@ alias pacman-remove-orphans="${_pacman_sudo}${_pacman_frontend} --remove --recur
 # Synchronizes the local package and Arch Build System databases against the
 # repositories.
 if (( $+commands[abs] )); then
-  alias pacu="${_pacman_sudo}${_pacman_frontend} --sync --refresh && sudo abs"
+  alias pacu="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --sync --refresh && sudo abs"
 else
-  alias pacu="${_pacman_sudo}${_pacman_frontend} --sync --refresh"
+  alias pacu="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --sync --refresh"
 fi
 
 # Synchronizes the local package database against the repositories then
 # upgrades outdated packages.
-alias pacU="${_pacman_sudo}${_pacman_frontend} --sync --refresh --sysupgrade"
+alias pacU="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --sync --refresh --sysupgrade"
 
 unset _pacman_{frontend,sudo}
+
+function paclist() {
+  # Source: https://bbs.archlinux.org/viewtopic.php?id=93683
+  LC_ALL=C pacman -Qei $(pacman -Qu | cut -d " " -f 1) | \
+    awk 'BEGIN {FS=":"} /^Name/{printf("\033[1;36m%s\033[1;37m", $2)} /^Description/{print $2}'
+}
+
+function pacdisowned() {
+  emulate -L zsh
+
+  tmp=${TMPDIR-/tmp}/pacman-disowned-$UID-$$
+  db=$tmp/db
+  fs=$tmp/fs
+
+  mkdir "$tmp"
+  trap  'rm -rf "$tmp"' EXIT
+
+  pacman -Qlq | sort -u > "$db"
+
+  find /bin /etc /lib /sbin /usr ! -name lost+found \
+    \( -type d -printf '%p/\n' -o -print \) | sort > "$fs"
+
+  comm -23 "$fs" "$db"
+}
+
+function pacmanallkeys() {
+  emulate -L zsh
+  curl -s https://www.archlinux.org/people/{developers,trustedusers}/ | \
+    awk -F\" '(/pgp.mit.edu/) { sub(/.*search=0x/,""); print $1}' | \
+    xargs sudo pacman-key --recv-keys
+}
+
+function pacmansignkeys() {
+  emulate -L zsh
+    for key in $*; do
+      sudo pacman-key --recv-keys $key
+      sudo pacman-key --lsign-key $key
+      printf 'trust\n3\n' | sudo gpg --homedir /etc/pacman.d/gnupg \
+        --no-permission-warning --command-fd 0 --edit-key $key
+    done
+}
