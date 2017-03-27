59084717ebcf1fba34a9c33da7f20f2aaafddb31
Andrew Bell <andrewbell8@gmail.com>
2017-03-27 06:44:47 -0500

    Update init.zsh

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -89,8 +89,6 @@ alias pacU="${_pacman_sudo}${_pacman_frontend}${_pacman_noconfirm} --sync --refr
 
 unset _pacman_{frontend,sudo}
 
-<<<<<<< HEAD
-=======
 alias pki='packer -S --noconfirm --noedit --devel --skipinteg'
 alias as='allservers'
 alias makef='makepkg --install --syndeps --force --skipinteg  --skipchecksums --skippgpcheck'
@@ -99,7 +97,6 @@ alias yr='yaourt -Rs --noconfirm'
 alias cs='cower --search --by name --color=always --ignore-ood --rsort popularity'
 alias bi='sudo bauerbill -Syu --noconfirm'
 
->>>>>>> 79d1cdefa89b61b50e001131795675db96272840
 function paclist() {
   # Source: https://bbs.archlinux.org/viewtopic.php?id=93683
   LC_ALL=C pacman -Qei $(pacman -Qu | cut -d " " -f 1) | \
@@ -131,11 +128,7 @@ function pacmanallkeys() {
     xargs sudo pacman-key --recv-keys
 }
 
-<<<<<<< HEAD
-function pacmansignkeys() {
-=======
 function pacsign() {
->>>>>>> 79d1cdefa89b61b50e001131795675db96272840
   emulate -L zsh
     for key in $*; do
       sudo pacman-key --recv-keys $key
