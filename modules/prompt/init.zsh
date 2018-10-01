e1f283b32c2c865fae20c2ad5dd9c3ebfe898342
Andrew Bell <bell.a.j@icloud.com>
2018-10-01 18:47:58 -0500

    switched all submodules to master branch

--- modules/prompt/init.zsh
+++ modules/prompt/init.zsh
@@ -11,8 +11,8 @@ autoload -Uz promptinit && promptinit
 # Load the prompt theme.
 zstyle -a ':prezto:module:prompt' theme 'prompt_argv'
 if [[ "$TERM" == (dumb|linux|*bsd*) ]] || (( $#prompt_argv < 1 )); then
-    prompt 'off'
+  prompt 'off'
 else
-    prompt "$prompt_argv[@]"
+  prompt "$prompt_argv[@]"
 fi
 unset prompt_argv
